c0cf9f1dbf89ed4380818f7b9d720bec69bb4829
cepholopoddreamz <leslie.plumb@gmail.com>
2019-01-31 16:37:13 +0000

    test change

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const usersRoutes = require("./routes/users");
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
-
+//fa la la 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
