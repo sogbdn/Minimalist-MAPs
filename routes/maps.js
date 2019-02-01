97d88abc7dc41efdb97d942c8f0da22e8904d859
sogbdn <sonia.barretdenazaris@gmail.com>
2019-02-01 23:32:07 +0000

    all files saved

--- routes/maps.js
+++ routes/maps.js
@@ -1,17 +1,17 @@
 "use strict";
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
     knex
       .select("*")
-      .from("users")
+      .from("maps")
       .then((results) => {
         res.json(results);
-    });
+      });
   });
 
   return router;
