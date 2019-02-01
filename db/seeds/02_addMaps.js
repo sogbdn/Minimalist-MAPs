a67d05799218e420f4351da61a196badfeb9b4bb
cepholopoddreamz <leslie.plumb@gmail.com>
2019-02-01 20:20:32 +0000

    added dumby data content

--- db/seeds/02_addMaps.js
+++ db/seeds/02_addMaps.js
@@ -5,9 +5,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('maps').insert([
-        { name: '', description: '', lat: '', lng: '', zoom: '', user_id: '' },
-        { name: '', description: '', lat: '', lng: '', zoom: '', user_id: '' },
-        { name: '', description: '', lat: '', lng: '', zoom: '', user_id: '' },
+        { name: 'map1', description: 'somewhere to rest', lat: '45.493965', lng: ' -73.576845', zoom: '15', user_id: '' },
+        { name: 'map2', description: 'somewhere to run', lat: '45.495935', lng: '-73.573444', zoom: '15', user_id: '' },
+        { name: 'map3', description: 'somewhere to hide', lat: '45.505857', lng: '-73.584183', zoom: '15', user_id: '' },
       ]);
     });
 };
