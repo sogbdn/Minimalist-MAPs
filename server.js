9d794660b41db3fcda3cfdea32c294dc56593205
sogbdn <sonia.barretdenazaris@gmail.com>
2019-02-02 19:25:29 +0000

    login done

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const knexLogger = require('knex-logger');
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 const mapsRoutes = require("./routes/maps");
+const indexRoutes = require("./routes/index");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -38,11 +39,10 @@ app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
-
 app.use("/api/maps", mapsRoutes(knex));
+app.use("/", indexRoutes(knex));
 
 // Home page
-
 
 
 app.listen(PORT, () => {
