f9285fcfa35284ffc270af47c0713be911aaea45
cepholopoddreamz <leslie.plumb@gmail.com>
2019-02-02 23:26:37 +0000

    trying to get the googleapijs file to appear in the pulls of other teammates git ignore isnt working properly

---
+++ public/styles/googleapi.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+
+function initMap() {
+  var map = new google.maps.Map
+  // $('.map')
+  (document.getElementById('map'), {
+    zoom: 17,
+    center: {lat: 45.496338, lng: -73.570732 }
+  });
+  map.addListener('click', function(e) {
+    placeMarkerAndPanTo(e.latLng, map);
+    //infowindow.setContent('Zoom: ' + map.getZoom());
+    //console.log(e);
+  });
+}
+
+function placeMarkerAndPanTo(latLng, map) {
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+    //infowindow.setContent('Zoom: ' + map.getZoom());
+  });
+  console.log(marker)
+  map.panTo(latLng);
+  console.log(latLng.lat());
+  console.log(latLng.lng());
+  }
+// })
