bb88fbab096ce709e773cfc7b093c21f1bf67f67
sogbdn <sonia.barretdenazaris@gmail.com>
2019-02-03 00:08:04 +0000

    redirect login to createmap

--- routes/users.js
+++ routes/users.js
@@ -7,18 +7,9 @@ module.exports = (knex) => {
 
   //Post to login to save a cookie
   router.post("/login/:id", (req, res) => {
-    req.session.user_id = request.params.id;
-    knex // Select the maps of the user
-      .select('*')
-      .from('maps')
-      .where('user' = user_id);
-
-    res.redirect("/mymaps");
+    req.session.user_id = req.params.id;
+    res.redirect("/createmap.html");
   });
-
-
-
-
 
   router.post("/logout", (req, res) => {
     req.session = null;
