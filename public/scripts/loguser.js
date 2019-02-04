1664b72575e86612928e57b0304621850203efc0
frann-y <fgranger00.com>
2019-02-04 23:13:18 +0000

    Got login working. The values are hardcoded to Bob. Fix that in login user.js. Must fix event handler for login.

--- public/scripts/loguser.js
+++ public/scripts/loguser.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
       const request = (options, cb) => {
         $.ajax(options)
           .done(response => {
-            cb(response);
+            cb(response); //will put the actions on success
+            //save session cookies
           })
           .fail(err => {
             console.log('Error: ', err);
@@ -19,11 +20,27 @@ $(document).ready(function() {
         event.preventDefault();
         let loggingText = $("form textarea");
 
-          if(loggingText.length > 10) {
-            alert('Too Long!')
-            //change alerts to RED star or bubble pop up
-          } else if (loggingText.length === 0) {
-            alert('There is NOTHING!') 
-          }
+          // if(loggingText.length > 10) {
+          //   alert('Too Long!')
+          //   //change alerts to RED star or bubble pop up
+          // } else if (loggingText.length === 0) {
+          //   alert('There is NOTHING!') 
+          // } else {
+            $.ajax({
+              type: "GET",
+              url: "api/users/login/Bob",
+              success: function(data, textStatus) {
+              
+                // data.redirect contains the string URL to redirect to
+                window.location.href = "/api/maps/allmaps";
+              }
+            })
+            // .done(results => { 
+              
+            //   // console.log('Results:', results) 
+            // })
+
+
+          //}
         })
     });
--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,33 @@ const router = express.Router();
 module.exports = (knex) => {
 
   //Post to login to save a cookie
-  router.post("/login/:id", (req, res) => {
-    req.session.user_id = req.params.id;
-    res.redirect("/createmap");
+  router.get("/login/:id", (req, res) => {
+    // req.session.user_id = req.params.id;
+
+    knex
+      .select('*')
+      .from('users')
+      .where({name: req.params.id})
+      .then((results) => {
+        if (results) {
+          //checking if the results (user name) exists 
+          req.session.user_id = results.id;
+          req.session.user_name = results.name;
+          res.status(200).send()
+        } else {
+          //insert into knex
+          //insert into users the user name and set it from the form
+          //look up documentation for insert 
+          knex ('users')
+            .insert({name: req.params.id})
+            .then(results => { //results is the whole entery. the results of the query
+              //user has been created, so must set the cookie 
+              req.session.user_id = results.id;
+              req.session.user_name = results.name;
+              res.status(200).send()
+            }) 
+        } //maybe error
+      });
   });
 
   router.post("/logout", (req, res) => {
